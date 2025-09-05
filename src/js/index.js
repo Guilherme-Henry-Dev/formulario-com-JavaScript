bf6fa8729b4f05dad7c2ebd898a795ed64aad237
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-09-05 20:41:07 -0300

    Adicionado uma funcionalidade de textar os campos obrigatorio. Agora o campo dos Nomes, so pode conter letras. O campo do email, so pode conter um email valido. O campo do telefone so pode conter numeros e seguindo o padrão de codigo de pais mais o dd

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,36 @@
 document.getElementById("formulario").addEventListener("submit", e => {
   e.preventDefault();
 
-  const campos = ["nome", "email", "telefone", "mensagem"];
-  let valido = true;
+  const regras = {
+    nome: {
+      regex: /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/,
+      msg: "Digite apenas letras"
+    },
+    email: {
+      regex: /^[a-zA-Z0-9._%+-]+@(gmail|hotmail|icloud|outlook|yahoo)\.[a-z]{2,}(\.[a-z]{2,})?$/,
+      msg: "Digite um e-mail válido"
+    },
+    telefone: {
+      regex: /^\+?\d{1,3}?\s?\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/,
+      msg: "Digite um telefone válido"
+    },
+    mensagem: {
+      regex: /.+/,
+      msg: "Campo obrigatório"
+    }
+  };
 
+  let valido = true;
   document.querySelectorAll(".erro").forEach(el => el.remove());
 
-  for (let id of campos) {
+  for (let id in regras) {
     const campo = document.getElementById(id);
     campo.classList.remove("valido", "invalido");
 
-    if (!campo.value.trim()) {
+    if (!regras[id].regex.test(campo.value.trim())) {
       valido = false;
       campo.classList.add("invalido");
-
-      campo.insertAdjacentHTML("afterend", `<span class="erro">Campo obrigatório</span>`);
+      campo.insertAdjacentHTML("afterend", `<span class="erro">${regras[id].msg}</span>`);
     } else {
       campo.classList.add("valido");
     }
